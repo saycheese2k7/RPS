7952fbf1005bd558728b4c4dadfca18efd786ae9
saycheese2k7 <241291587+saycheese2k7@users.noreply.github.com>
2025-11-08 12:13:49 +0600

    game build done

--- script.js
+++ script.js
@@ -27,14 +27,33 @@ function getUserChoice() {
 function playRound(user, computer){
 
     if(user===computer){
-        console.log("Tie!")
+        console.log("Tie!");
     }
     else if(user==='rock' && computer==='scissor'){
-        console.log("You win!")
-        return userscore += 1
+        console.log("You win!");
+        return userscore += 1;
     }
     else if(user==='rock' && computer==='paper'){
-        console.log("You lose!")
-        return computerscore += 1
+        console.log("You lose!");
+        return computerscore += 1;
     }
+    else if(user==='paper' && computer==='scissor'){
+        console.log("You lose!");
+        return computerscore += 1;
+    }
+    else if(user==='paper' && computer==='rock'){
+        console.log("You win!")
+        return userscore += 1;
+    }
+    else if(user==='scissor' && computer==='rock'){
+        console.log("You lose!");
+        return computerscore += 1;
+    }
+    else{
+        console.log("You win!")
+        return userscore += 1;
+    }
+    
 }
+
+playRound(userchoice, computerchoice)
